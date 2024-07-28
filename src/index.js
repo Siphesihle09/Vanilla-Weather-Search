cb278839fd6bbeb2ec5f0309b632ed9927494d71
Siphesihle09 <siphe.mkhwanazi07@gmail.com>
2024-07-28 16:21:34 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let descriptionElement = document.querySelector("#description");
+  let timeElement = document.querySelector("present-time");
+  let date = new Date(response.data.time * 1000);
+  console.log(response.data);
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  descriptionElement.innerHTML = `${response.data.condition.description}.`;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function searchEngine(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
+function searchCity(city) {
+  let apiKey = "f9c18fe9ad39b36a26o47004111c3tcf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
 }
 
 let searchForm = document.querySelector("#search-form");
